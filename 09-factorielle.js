0872e534614a19bf45c69cc9527921db685cc8c2
Kis-lander <abeltenda.gmail.com>
2025-10-23 09:52:02 +0100

    feat : exercise 9 has been done

--- 09-factorielle.js
+++ 09-factorielle.js
@@ -12,8 +12,26 @@
 function factorielle(n) {
   // Exercice non implémenté : calculer la factorielle de n
   // Placeholder neutre : retourne 0 pour indiquer non-implémentation
-  return 0;
+  
+  // Vérifie que n est un nombre entier non négatif
+  if (typeof n !== "number" || n < 0 || !Number.isInteger(n)) {
+    return 0;
+  }
+  // Cas de base : 0! = 1
+  if (n === 0) {
+    return 1;
+  }
+  // Calcul itératif de la factorielle
+  let resultat = 1;
+  for (let i = 1; i <= n; i++) {
+    resultat *= i;
+  }
+  return resultat;
 }
+// Exemple d'utilisation :
+console.log(factorielle(5));
+console.log(factorielle(-2));
+console.log(factorielle("test"));
 
 // Ne pas modifier la ligne ci-dessous
 module.exports = { factorielle }
