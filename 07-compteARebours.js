86413c9a44de3c8f50858865047d7a13e1a090a9
Kis-lander <abeltenda.gmail.com>
2025-10-23 09:03:47 +0100

    feat exercise 7 has been done

--- 07-compteARebours.js
+++ 07-compteARebours.js
@@ -11,7 +11,7 @@
     sans lever d'exception. L'étudiant doit remplacer la fonction par son implémentation.
   */
   function compteARebours() {
-    // Placeholder neutre : ne fait rien
+    for (let i = 10; i >= 1; i--) {}
   }
 
   // Ne pas modifier la ligne ci-dessous
